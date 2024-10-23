786998d36dbb28aa7f0d9b8a2016e32354c0da11
Astborg <93790636+Astborg@users.noreply.github.com>
2024-10-23 12:14:37 +0200

    sql cronjob

--- backend/utils/cronJobs.js
+++ backend/utils/cronJobs.js
@@ -5,7 +5,9 @@ exports.runDailyJob = () => {
   cron.schedule('*/5 * * * *', () => {
     console.log('Cron job is running every 5 minutes');
 
-    const sql = 'UPDATE Annons SET Role = "old" WHERE Date < (CURDATE() - INTERVAL 1 DAY)';
+    
+    const sql = "UPDATE Annons SET Role = 'old' WHERE Date < (CURDATE() - INTERVAL 1 DAY)";
+    console.log(`Running SQL: ${sql}`);
     db.query(sql, (err, result) => {
       if (err) {
         console.error('Error updating old ads:', err.message);
