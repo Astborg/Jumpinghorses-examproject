eb9fbd72714cf658b2997a95d831daf58326c0ac
Astborg <93790636+Astborg@users.noreply.github.com>
2025-01-02 18:09:04 +0100

    express.raw

--- backend/routes/webhookRoutes.js
+++ backend/routes/webhookRoutes.js
@@ -4,6 +4,6 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-router.post('/webhook', bodyParser.raw({ type: 'application/json' }), handleWebhook);
+router.post('/webhook', express.raw({ type: 'application/json' }), handleWebhook);
 
 module.exports = router;
